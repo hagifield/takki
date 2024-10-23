f7a446007bfad683a69148d4a44f464437afb3ce
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-10-23 10:39:30 +0000

    created comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :public do
+    get 'comments/create'
+    get 'comments/destroy'
+  end
+  namespace :public do
     get 'ownerships/create'
     get 'ownerships/destroy'
   end
