076e39fabfe5fb8150eedbd93aaac6cf32635541
EC2 Default User <ec2-user@ip-172-31-34-85.ap-northeast-1.compute.internal>
2024-10-23 11:20:52 +0000

    editted routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,70 @@
 Rails.application.routes.draw do
+  # Public側
+  scope module: :public do
+    # Homes
+    root to: 'homes#top' # トップページ
+    get 'about', to: 'homes#about'
+
+    # Users
+    get 'mypage', to: 'users#mypage'
+    resources :users, only: [:index, :show, :edit, :update, :destroy] do
+      member do
+        get 'followers'
+        get 'followings'
+      end
+    end
+
+    # Posts
+    resources :posts do
+      collection do
+        get 'likes', to: 'posts#like_index'
+      end
+      resources :comments, only: [:create, :destroy] # ポストに対するコメント
+    end
+
+    # Tickets
+    resources :tickets do
+      collection do
+        get 'users/:id', to: 'tickets#my_tickets', as: 'user_tickets' # 自分の所持しているチケット一覧
+      end
+      member do
+        get 'transfer'
+      end
+      resources :comments, only: [:create, :destroy] # チケットに対するコメント
+    end
+
+    # Notifications
+    resources :notifications, only: [:index, :show, :destroy]
+
+    # Likes
+    resources :likes, only: [:create, :destroy] do
+      collection do
+        get 'users/:id/posts', to: 'likes#index_posts', as: 'user_post_likes'
+        get 'users/:id/tickets', to: 'likes#index_tickets', as: 'user_ticket_likes'
+      end
+    end
+
+    # Ownerships
+    resources :ownerships, only: [:create, :destroy]
+
+    # Relationships (follows/unfollows)
+    resources :relationships, only: [:create, :destroy]
+  end
+
+  # Adminネームスペース
   namespace :admin do
-    get 'admin_notifications/new'
-    get 'admin_notifications/create'
-    get 'admin_notifications/index'
-    get 'admin_notifications/show'
-    get 'admin_notifications/edit'
-    get 'admin_notifications/update'
-    get 'admin_notifications/destroy'
+    # Admin Users
+    resources :users, only: [:index, :show, :edit, :update, :destroy] do
+      member do
+        get 'followers'
+        get 'followings'
+      end
+    end
+
+    # Admin notifications
+    resources :admin_notifications, only: [:new, :create, :index]
   end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/destroy'
-  end
-  namespace :public do
-    get 'relationships/create'
-    get 'relationships/destroy'
-  end
-  namespace :public do
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'ownerships/create'
-    get 'ownerships/destroy'
-  end
-  namespace :public do
-    get 'likes/index_posts'
-    get 'likes/index_tickets'
-    get 'likes/create'
-    get 'likes/destroy'
-  end
-  namespace :public do
-    get 'notifications/index'
-    get 'notifications/show'
-    get 'notifications/destroy'
-  end
-  namespace :public do
-    get 'tickets/new'
-    get 'tickets/create'
-    get 'tickets/show'
-    get 'tickets/index'
-    get 'tickets/transfer'
-    get 'tickets/my_tickets'
-    get 'tickets/edit'
-    get 'tickets/update'
-    get 'tickets/destroy'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/create'
-    get 'posts/show'
-    get 'posts/index'
-    get 'posts/edit'
-    get 'posts/update'
-    get 'posts/destroy'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
+  
   devise_for :admins, controllers: {
   registrations: "admin/registrations",
   sessions: "admin/sessions"
